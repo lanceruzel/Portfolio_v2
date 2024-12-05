e5ec33b7cff5614b16be00fd5c51479f8d21459b
LanceRuzel <lanceruzel2@gmail.com>
2024-12-05 23:54:07 +0800

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         'secondary': '#0353A4',
         'tartiary': '#B9D6F2',
       },
+      container: {
+        padding: '1rem', // Add padding to mimic Bootstrap's behavior
+      },
+    },
+    screens: {
+      sm: '576px',   // Small screens and up
+      md: '768px',   // Medium screens and up
+      lg: '992px',   // Large screens and up
+      xl: '1200px',  // Extra large screens and up
+      xxl: '1400px', // 2XL screens and up
     },
   },
   plugins: [],
