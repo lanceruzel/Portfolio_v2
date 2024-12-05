0b2b68177fa16cf40188de968b1080475c464589
LanceRuzel <lanceruzel2@gmail.com>
2024-12-05 10:33:15 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#061A40',
+        'secondary': '#0353A4',
+        'tartiary': '#B9D6F2',
+      },
+    },
+  },
+  plugins: [],
+}
+
